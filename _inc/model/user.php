c0c7de1284d22b8b4838b98dcd6c4e4ad78acc4b
omartru10 <trujillo.0643@gmail.com>
2023-07-27 22:19:39 -0400

    add field ip

--- _inc/model/user.php
+++ _inc/model/user.php
@@ -17,8 +17,8 @@ class ModelUser extends Model
 	public function addUser($data) 
 	{
 		
-    	$statement = $this->db->prepare("INSERT INTO `users` (username, email, mobile, password, raw_password, group_id, dob, user_image, created_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
-    	$statement->execute(array($data['username'], $data['email'], $data['mobile'], md5($data['password']), $data['password'], (int)$data['group_id'], $data['dob'], $data['user_image'], date_time()));
+    	$statement = $this->db->prepare("INSERT INTO `users` (username, email, mobile, password, raw_password, group_id, dob, user_image,ip, created_at) VALUES (?, ?, ?, ?, ?, ?, ?,?, ?, ?)");
+    	$statement->execute(array($data['username'], $data['email'], $data['mobile'], md5($data['password']), $data['password'], (int)$data['group_id'], $data['dob'], $data['user_image'],getIp(), date_time()));
 
     	$id = $this->db->lastInsertId();
 
