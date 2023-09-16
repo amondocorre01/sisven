ff1fa85e6330d6560919fc51f4c07a15c81cf9d7
amondocorre <amondocorre@capressocafe.com>
2023-09-15 20:18:23 -0400

    20230911-1520

--- assets/itsolution24/jsmin/pos.js
+++ assets/itsolution24/jsmin/pos.js
@@ -10937,9 +10937,9 @@ window.angularApp.factory("PaymentFormModal", ["API_URL", "window", "jQuery", "$
                             } else if (window.settings.after_sell_page == 'toastr_msg') {
                                 window.toastr.success("ID: "+$scope.invoiceId, "Success!");
                             } else if (window.settings.after_sell_page == 'sweet_alert_msg') {
-                                window.swal("Success.", "ID: "+$scope.invoiceId, "success");
+                                window.swal("Success..", "ID: "+$scope.invoiceId, "success");
                             } else {
-                                window.swal("Success.", "ID: "+$scope.invoiceId, "success");
+                                window.swal("Success..", "ID: "+$scope.invoiceId, "success");
                             }
                         }
 
